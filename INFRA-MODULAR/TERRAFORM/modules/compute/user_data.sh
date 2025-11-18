f75581a0c7884590f663edeaa33a0b4a54d18ba3
SUPPP <jmirasfranchi@gmail.com>
2025-11-17 22:13:59 -0300

    asd
    asd

--- INFRA-MODULAR/TERRAFORM/modules/compute/user_data.sh
+++ INFRA-MODULAR/TERRAFORM/modules/compute/user_data.sh
@@ -12,12 +12,18 @@ if ! command -v aws >/dev/null 2>&1; then
   yum install -y awscli
 fi
 
+echo $(pwd)
+mkdir -p /home/ec2-user/obligatorio/
+cd /home/ec2-user/obligatorio/
+git clone https://github.com/Joaquin1899/obligatorio-test 
+mkdir -p /home/ec2-user/obligatorio/html
+cd /home/ec2-user/obligatorio/html
+git clone https://github.com/ORT-FI-7417-SolucionesCloud/e-commerce-obligatorio-2025 
 
-git pull https://github.com/lsanguinetti11/OBLIGATORIO.git /home/ec2-user/obligatorio
-git pull https://github.com/ORT-FI-7417-SolucionesCloud/e-commerce-obligatorio-2025 /home/ec2-user/obligatorio/html
 cd /home/ec2-user/obligatorio
 docker build -t obligatorio .
 docker run -d -p 8080:80 --name obligatorio obligatorio
 
 
 #BORRAR ECR
+
